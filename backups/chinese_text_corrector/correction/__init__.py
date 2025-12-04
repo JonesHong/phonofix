1d49ab4d78c45c9bff91407da1024e0dbabb0e7c
JonesHong <latte831104@gmail.com>
2025-12-04 21:52:22 +0800

    chore: remove backups/ from git tracking (already in .gitignore)

--- backups/chinese_text_corrector/correction/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-校正模組 - 文本校正功能
-
-提供 ASR 後處理的文本校正功能
-"""
-
-from .corrector import ChineseTextCorrector
-
-__all__ = ["ChineseTextCorrector"]
--- backups/chinese_text_corrector/dictionary/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-詞典模組 - 模糊音詞典生成
-
-提供模糊音詞典生成和同音詞過濾功能
-"""
-
-from .generator import FuzzyDictionaryGenerator
-
-__all__ = ["FuzzyDictionaryGenerator"]
